14c2a3da91dcc39b19762bc3a843405b5314254d
MrFreemanes <savazak3@gmail.com>
2025-10-21 19:45:56 +0300

    ADD: classes Task, Result in config.py

--- config/config.py
+++ config/config.py
@@ -1,5 +1,22 @@
+from dataclasses import dataclass
+
+
 class Status:
     """Используется в Bridge и в worker"""
     RUN = 'run'
     DONE = 'done'
     ERROR = 'error'
+
+
+@dataclass(repr=True)
+class Task:
+    task: str
+    new_path: str = None
+
+
+@dataclass(repr=True)
+class Result:
+    data: dict
+    status: str
+    progress: int
+    text_error: str = None
